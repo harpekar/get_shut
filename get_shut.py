cd1d3e131135b6ed9dfb01c69596ad39ea57564f
Karen <karenl.harper88@gmail.com>
2020-08-01 16:30:06 -0700

    Added error handling if no results are found

--- get_shut.py
+++ get_shut.py
@@ -14,8 +14,10 @@ url = 'http://www.shutterstock.com/search/' + search_term
 page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Mobile Safari/537.36'})
 
 #Get the link for the first resulting image 
-img_url = BeautifulSoup(page.content, "html.parser").find("img").get('src')
+try: img_url = BeautifulSoup(page.content, "html.parser").find("img").get('src')
 
-img = Image.fromarray(imread(img_url))
+except: print("No image results for that search")
 
-img.show()
+else:
+    img = Image.fromarray(imread(img_url))
+    img.show()
